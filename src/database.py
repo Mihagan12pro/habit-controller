1cebc2ab42bd2d24503caabc381ca1fa520d68d0
guiltylord <desxzawq@gmail.com>
2025-12-09 19:22:48 +0300

    chore: replace db Base class to connection with db

--- src/database.py
+++ src/database.py
@@ -1,22 +1,18 @@
 # src/database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
-# 1. Меняем строку подключения. 
-# "sqlite:///./flawless.db" означает, что файл создастся в текущей папке.
 SQLALCHEMY_DATABASE_URL = "sqlite:///./flawless.db"
 
-# 2. Создаем движок
-# connect_args={"check_same_thread": False} — ЭТО ВАЖНО ТОЛЬКО ДЛЯ SQLITE
-# (нужно, чтобы FastAPI мог работать с базой в многопоточном режиме)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# Создаем базовый класс в современном стиле 2.0
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()
