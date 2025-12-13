5598830c955b0c2a8db7eb0374d8e3bdd1c525d6
guiltylord <desxzawq@gmail.com>
2025-12-13 14:26:51 +0300

    feat: create app in app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,16 @@
-# from models.Base import Base
-# from database import engine
+from fastapi import FastAPI
+from src.database import engine, Base
+from src.routers import user, habit
 
-# Base.metadata.create_all(bind=engine)
+# Создаем таблицы при старте
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Flawless App API")
+
+# Подключаем роутеры
+app.include_router(user.router)
+app.include_router(habit.router)
+
+@app.get("/")
+def root():
+    return {"message": "Flawless API is running!"}
