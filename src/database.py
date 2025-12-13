536257d1bcac54adfe02ba4d0e3e7e5cf8096523
Михаил Каргин <kargin.miha@outlook.com>
2025-12-13 11:05:07 +0300

    feat: add RepositoryBase class to the repositories folder

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./flawless.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./habitsController.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -19,4 +19,6 @@ def get_db():
     try:
         yield db
     finally:
-        db.close()
+        db.close()#Не лучше ли перенести это в папку "models"? 
+
+Base.metadata.create_all(bind=engine)
