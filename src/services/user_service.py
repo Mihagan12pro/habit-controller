5ae43f10e0edb9e469983925d9a6bcfc8b6e3683
Mihagan12pro <kargin.miha@outlook.com>
2025-12-20 19:03:42 +0300

    feat: use check_errors in every function in users_service

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -14,9 +14,7 @@ from src.repositories.users import UsersRepository
 async def create_user(db: AsyncSession, user_dto: schemas.UserCreate) -> Union[int, str]:
     users_repo = UsersRepository(db)
     result = await users_repo.create(user_dto)
-
-    if isinstance(result, str):
-        raise HTTPException(status_code=409, detail=result)
+    check_errors(result, 409)
 
     return result
 
@@ -26,7 +24,12 @@ async def get_user_by_id(db: AsyncSession, user_id: int):
     result = await users_repo.get_by_id(user_id)
     check_errors(result, 404)
 
+    return result
+
 
 async def get_user_by_email(db: AsyncSession, email: str):
     users_repo = UsersRepository(db)
-    return await users_repo.get_by_email(email)
+    result = await users_repo.get_by_email(email)
+    check_errors(result, 404)
+
+    return result
