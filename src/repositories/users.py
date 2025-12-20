802a6579b8c3d1178b8c50f934ee845471515135
Mihagan12pro <kargin.miha@outlook.com>
2025-12-20 15:09:27 +0300

    fix: creating new user

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,31 +1,46 @@
-from typing import Optional
+from typing import Optional, Union
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.user import User
 from src.repositories.base import RepositoryBase
+import src.schemas as dto
 
 
 class UsersRepository(RepositoryBase):  # Репозиторий для юзеров
     def __init__(self, session: AsyncSession):
         super().__init__(session)
 
-    async def get_by_email(self, email: str) -> Optional[User]:
+    async def get_by_email(self, email: str) -> Union[User, str]:
         """Получить пользователя по email"""
         stmt = select(User).where(User.email == email)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
+    """
+    Получить пользователя по id
+    """
     async def get_by_id(self, user_id: int) -> Optional[User]:
-        """Получить пользователя по id"""
         stmt = select(User).where(User.id == user_id)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def create(self, user: User) -> User:
-        """Создать нового пользователя"""
+    """
+    Создание нового пользователя
+    """
+    async def create(self, user_dto: dto.UserCreate) -> Union[int, str]:
+        user = User()
+        user.email = user_dto.email
+        user.name = user_dto.name
+        user.hashed_password = user_dto.password
+
+        email_result = await self.get_by_email(user.email)
+
+        if isinstance(email_result, User) == False:
+            return 'Данная почта уже занята!'
+                
         self.session.add(user)
         await self.session.commit()
         await self.session.refresh(user)
-        return user
+        return user.id
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,26 +9,10 @@ from src.models.user import User
 from src.repositories.users import UsersRepository
 
 
-async def create_user(db: AsyncSession, user_dto: schemas.UserCreate):
+async def create_user(db: AsyncSession, user_dto: schemas.UserCreate) -> Union[int, str]:
     users_repo = UsersRepository(db)
-    
-    # Проверяем, занят ли email
-    existing_user = await users_repo.get_by_email(user_dto.email)
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    
-    # Тут можно добавить хеширование пароля
-    fake_hashed_pw = user_dto.password + "notreallyhashed"
-
-    new_user = User(
-        name=user_dto.name, email=user_dto.email, hashed_password=fake_hashed_pw
-    )
-    
-    try:
-        return await users_repo.create(new_user)
-    except IntegrityError:
-        await db.rollback()
-        raise HTTPException(status_code=400, detail="Email already registered")
+    result = await users_repo.create(user_dto)
+    return result
 
 
 async def get_user_by_id(db: AsyncSession, user_id: int):
