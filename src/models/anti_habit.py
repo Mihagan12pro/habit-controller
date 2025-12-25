fde5b1abd96a302262dabd472380dd421cace767
guiltylord <desxzawq@gmail.com>
2025-12-25 13:19:28 +0300

    feat: add antihabit model

--- src/models/anti_habit.py
+++ src/models/anti_habit.py
@@ -1,10 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.models.user import User  # pragma: no cover
 
 
 class AntiHabit(Base):
-    __tablename__ = "anti_habits_table"
+    __tablename__ = "anti_habits"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    pass
+    title: Mapped[str] = mapped_column(String, index=True)
+    started_at: Mapped[datetime]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="anti_habits")
